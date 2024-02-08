ef17e3e5793d691322e199edfa1f91502fdd9411
Anna K <anita.explorer@gmail.com>
2024-02-08 18:03:44 +0100

    Sounds added

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener('click', function () {
-        alert("I got clicked!")
+
+        switch (this.innerHTML) {
+            case "w":
+                new Audio('./sounds/crash.mp3').play()
+                break;
+            case "a":
+                new Audio('./sounds/kick-bass.mp3').play()
+                break;
+            case "s":
+                new Audio('./sounds/snare.mp3').play()
+                break;
+            case "d":
+                new Audio('./sounds/tom-1.mp3').play()
+                break;
+            case "j":
+                new Audio('./sounds/tom-2.mp3').play()
+                break;
+            case "k":
+                new Audio('./sounds/tom-3.mp3').play()
+                break;
+            case "l":
+                new Audio('./sounds/tom-4.mp3').play()
+                break;
+            default:
+                console.log(this.innerHTML)
+                break;
+        }
+
+
+
+        this.style.color = 'white'
     })
 }
-
+function playAudio() {
+    var audioTom1 = new Audio('./sounds/tom-1.mp3')
+    return audioTom1.play()
+}
